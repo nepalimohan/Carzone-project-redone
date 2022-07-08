770aeb225a71c056bff5a00a1e1325ec956222fd
nepalimohan <nepalimohan@outlook.com>
2022-07-08 15:32:55 +0545

    team admin customization

--- Carzone/urls.py
+++ Carzone/urls.py
@@ -1,7 +1,11 @@
+from xml.dom.minidom import Document
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
