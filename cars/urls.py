df32f9300d287df66c6adc1f9990caaec8f95f8a
nepalimohan <nepalimohan@outlook.com>
2022-07-11 18:45:36 +0545

    Single page cars completed

--- cars/urls.py
+++ cars/urls.py
@@ -1,6 +1,8 @@
+from unicodedata import name
 from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.cars, name='cars')
+    path('', views.cars, name='cars'),
+    path('<int:id>', views.car_details, name="car_details"),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Car
 
 # Create your views here.
 def cars(request):
     return render(request, 'cars/cars.html')
+
+def car_details(request, id):
+    single_car = get_object_or_404(Car, pk=id)
+    data = {
+        'single_car':single_car,
+    }
+    return render(request, 'cars/car_details.html', data)
