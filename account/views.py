8058f722ad6cee8aea11c144dffc30033e61298b
nepalimohan <nepalimohan@outlook.com>
2022-07-18 13:16:16 +0545

    LoginPageCreated

--- account/views.py
+++ account/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def login(request):
+    return render(request, 'account/login.html')
+
+def logout(request):
+    return redirect('home')
+
+def register(request):
+    return render(request, 'account/register.html')
+
+def dashboard(request):
+    return render(request, 'account/dashboard.html')
