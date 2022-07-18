091a69d8fc9bf07740d7af2860433d82f1a9f09a
nepalimohan <nepalimohan@outlook.com>
2022-07-18 22:03:42 +0545

    AccountPAgesUpdated

--- account/views.py
+++ account/views.py
@@ -8,6 +8,8 @@ def logout(request):
     return redirect('home')
 
 def register(request):
+    if request.method == 'POST':
+        print("demo")
     return render(request, 'account/register.html')
 
 def dashboard(request):
