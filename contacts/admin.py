07ad32e4ff8a282bf4c3104d640e9d373d2905ab
nepalimohan <nepalimohan@outlook.com>
2022-07-25 11:01:05 +0545

    AdminRegistered

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Inquiry
 
 # Register your models here.
+@admin.register(Inquiry)
+class InquiryAdmin(admin.ModelAdmin):
+    list_display = ['id','first_name','last_name','email','car_title','city','created_date']
+    list_display_links = ['id','first_name', 'last_name','email' ]
+    search_fields = ['first_name', 'last_name','email' ,'car_title'] 
+    list_per_page = 25
