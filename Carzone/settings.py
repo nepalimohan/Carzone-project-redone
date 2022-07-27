20bf65b459074c28165cf22b4820c4f328a0a140
nepalimohan <nepalimohan@outlook.com>
2022-07-27 11:43:33 +0545

    SendingMails

--- Carzone/settings.py
+++ Carzone/settings.py
@@ -169,7 +169,7 @@ SITE_ID = 1
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'nepali.mohan11@gmail.com'
-EMAIL_HOST_PASSWORD = 'sofpixbfyigzklcg'
+EMAIL_HOST_PASSWORD = 'zaaehvytqhxyatda'
 EMAIL_USE_TLS = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Team
 from cars.models import Car
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -35,4 +38,28 @@ def services(request):
     return render(request, 'pages/services.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        
+        admin_info = User.objects.get(is_superuser=True)
+        admin_email = admin_info.email
+        
+        email_subject = 'You have a new message from Carzone regarding' + subject
+        message_body = 'Name: ' + name + '. Email:' + email + '. Phone: ' + phone + '. Message: ' + message
+        
+        send_mail(
+                email_subject,
+                message_body,
+                'nepali.mohan11@gmail.com',
+                [admin_email],
+                fail_silently=False,
+                )
+        messages.success(request, "Your messgae has been received. We will get back to you shortly!!!")
+        return redirect('contact')
+        
+        
     return render(request, 'pages/contact.html')
